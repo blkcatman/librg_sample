b21b1c539e7c007e9754816e1b9e5d2a9d2d2dcd
blkcatman <tatsuro.matsubara@gmail.com>
2023-08-19 22:09:50 +0900

    update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+pushd "src"
+wget -q "https://github.com/zpl-c/librg/releases/download/v7.2.1/librg.h" -O librg.h
+popd
+
 mkdir build -p
 pushd "build"
-cmake .. & make
+cmake ..
+make
 chmod +x example
 popd
